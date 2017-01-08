4f409f3fa87a6a8513d1f7ddfec7c144aa5ed6e2
Daniel Marreco <daniel.marreco@gmail.com>
2017-01-08 15:51:12 +0000

    cenário feliz com respostas mockeadas

--- examples/quickstart.js
+++ examples/quickstart.js
@@ -40,6 +40,101 @@ const actions = {
     const {text, quickreplies} = response;
     console.log('sending...', JSON.stringify(response));
   },
+  
+  /*
+   * BENIE_APLHA Example
+   */
+  getProduct({context, entities}) {
+    var product = firstEntityValue(entities, 'product');
+    if (product) {
+      context.productInfo = {
+        name: "Sorine",
+        presentation: "0,5 MG/ML SOL NAS CT FR PLAS TRANS GOT X 30 ML",
+        activeIngredient: "Cloridrato de Nafazolina",
+        usage: "Descongestionantes Nasais Tópicos",
+      };
+      delete context.missingProduct;
+    } else {
+      context.missingProduct = true;
+      delete context.forecast;
+    }
+    return context;
+  },
+  
+  getQuotation({context, entities}) {
+    context.quotation = {
+      items: [
+        {
+          caterer: "Drogasmil",
+          price: "11.90"
+        },
+        {
+          caterer: "Pacheco",
+          price: "12.70"
+        },
+        {
+          caterer: "Raia",
+          price: "15.10"
+        }
+      ]
+    };
+    return context;
+  },
+  
+  getPaymentOptions({context}) {
+    var paymentOptions = {
+      items:[
+        {name: "Dinheiro"},
+        {name: "Débito"},
+        {name: "Crédito"}
+      ]
+    };
+    context.paymentOptions = paymentOptions;
+    return context;
+  },
+  
+  getCurrentOrder({context}) {
+    
+    var cart = {
+      items: [
+        {
+          productName: "SORINE - 0,5 MG/ML SOL NAS CT FR PLAS TRANS GOT X 30 ML ",
+          price: 11.90
+        }
+      ],
+      totalCost: 11.90,
+      catererId: 1,
+      catererName: "Drogasmil"
+    };
+    
+    var orderInfo = {
+      customerName: "Daniel Marreco",
+      customerAddress: "Rua Canning, 22 ap 104 - Ipanema",
+      customerPhone: "(21)981040760",
+      cart: cart
+    };
+    
+    context.orderInfo = orderInfo;
+    
+    return context;
+  },
+  
+  
+  selectCaterer({context, entities}) {
+    var selectedCaterer = firstEntityValue(entities, 'caterer');
+    context.caterer = selectedCaterer;
+    return context;
+  },
+  
+  selectPaymentForm({context, entities}) {
+    var selectPaymentForm = firstEntityValue(entities, 'paymentForm');
+    context.paymentForm = selectPaymentForm;
+    return context;
+  },
+  
+  /*
+   * WEATHER example
+   */
   getForecast({context, entities}) {
     var location = firstEntityValue(entities, 'location');
     if (location) {
